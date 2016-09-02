64d549255bcccd64e32ad3233a7513b34f7744aa
webappm <tonsgawangwork@gmail.com>
2016-09-02 11:29:19 +0800

    v1.21
    正式版

--- loginlink.js
+++ loginlink.js
@@ -1,1 +1,1 @@
-1.21,aHR0cDovL3d3dy5teWJtd2NsdWIuY24v,aHR0cDovL2Jicy5ibXdza3kuY29tLw==,aHR0cDovL3d3dy53ZWliby5jb20vbXlibXdjbHVi
+1.21,aHR0cDovL3d3dy5ibXd2aXAub3JnLz9sb2dpbmFwcA==,aHR0cDovL3d3dy5ibXc2ODY2LmNvbS8/bG9naW5hcHA=,aHR0cDovL3d3dy5ibXc2OTY2LmNvbS8/bG9naW5hcHA=,aHR0cHM6Ly93d3cuYm13MzY4LmNvbS8/bG9naW5hcHA=
